3f633c8e2897863ca1bfadf5064bcc5f9683982e
J Xander Crawford <49574566+jxandercrawford@users.noreply.github.com>
2022-05-06 11:41:07 -0400

    Added some comments to script

--- NewsAPI.py
+++ NewsAPI.py
@@ -4,9 +4,11 @@ from flask import request, jsonify
 from NewsModules import NewsDataFeed
 from miscModules import sectionDump
 
+# Initialize app
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+# Home page
 @app.route('/', methods=['GET'])
 def home():
     return '''
@@ -14,15 +16,17 @@ def home():
     <p>Get the lastest stories here!</p>
     '''
 
-
+# NYT page
 @app.route('/api/nyt', methods=['GET'])
 def api_nyt():
 
+    # Initialize available sections and default output
     sections = ["Africa", "Americas", "ArtandDesign", "Arts", "AsiaPacific", "Automobile", "Baseball", "Books", "Business", "Climate", "CollegeBasketball", "CollegeFootball", "Dance", "Dealbook", "DiningandWine", "Economy", "Education", "EnergyEnvironment", "Europe", "FashionandStyle", "Golf", "Health", "Hockey", "HomePage", "Jobs", "Lens", "MediaandAdvertising", "MiddleEast", "MostEmailed", "MostShared", "MostViewed", "Movies", "Music", "NYRegion", "Obituaries", "PersonalTech", "Politics", "ProBasketball", "ProFootball", "RealEstate", "Science", "SmallBusiness", "Soccer", "Space", "Sports", "SundayBookReview", "Sunday-Review", "Technology", "Television", "Tennis", "Theater", "TMagazine", "Travel", "Upshot", "US", "Weddings", "Well", "YourMoney"]
     formatOutput = "json"
 
     args = request.args
 
+    # Parse arguments
     if "section" in args:
         section = args["section"]
 
@@ -32,6 +36,7 @@ def api_nyt():
 
         nytFeed = NewsDataFeed("nyt")
 
+        # Execute request
         try:
             if "search" in args:
                 searchterm = args["search"]
@@ -40,14 +45,15 @@ def api_nyt():
 
             return nytFeed.getFeed(section + ".xml", formatOutput)
         except:
+            # Error statement on request issue
             return f'''
             <h1>Unable to to get NYT feed</h1>
             <p>Try specifing a section in your query. Valid sections are:</p>
             {sectionDump(sections)}
             Additionally you can specify a format in your query ("json", "xml", "csv", "html")
             '''
-
     else:
+        # Error statement for no section
         return f'''
         <h1>No NYT section was specified</h1>
         <p>Try specifing a section in your query. Valid sections are:</p>
@@ -55,15 +61,17 @@ def api_nyt():
         Additionally you can specify a format in your query ("json", "xml", "csv", "html")
         '''
 
-
+# WSJ page
 @app.route('/api/wsj', methods=['GET'])
 def api_wsj():
 
+    # Initialize available sections and default output
     sections = ["RSSOpinion", "RSSWorldNews", "WSJcomUSBusiness", "RSSMarketsMain", "RSSWSJD", "RSSLifestyle"]
     formatOutput = "json"
     
     args = request.args
 
+    # Parse arguments
     if "section" in args:
         section = args["section"]
 
@@ -73,6 +81,7 @@ def api_wsj():
 
         wsjFeed = NewsDataFeed("wsj")
 
+        # Execute request
         try:
             if "search" in args:
                 searchterm = args["search"]
@@ -82,14 +91,15 @@ def api_wsj():
             
             return wsjFeed.getFeed(section + ".xml", formatOutput)
         except:
+            # Error statement on request issue
             return f'''
             <h1>Unable to to get WSJ feed</h1>
             <p>Try specifing a section in your query. Valid sections are:</p>
             {sectionDump(sections)}
             Additionally you can specify a format in your query ("json", "xml", "csv", "html")
             '''
-
     else:
+        # Error statement for no section
         return f'''
         <h1>No WSJ section was specified</h1>
         <p>Try specifing a section in your query. Valid sections are:</p>
@@ -97,5 +107,5 @@ def api_wsj():
         Additionally you can specify a format in your query ("json", "xml", "csv", "html")
         '''
 
-
-app.run()
+if __name__ == "__main__":
+    app.run()
